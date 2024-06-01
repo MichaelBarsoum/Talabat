5dd3a0dbb48346376a2f967b110d34872f7935a1
Michael <acc.MichaelNabil729@gmail.com>
2024-06-01 23:21:38 +0300

    RemoveAy7aga

--- Talabat.Core/obj/Debug/net6.0/Talabat.Core.AssemblyInfo.cs
+++ Talabat.Core/obj/Debug/net6.0/Talabat.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c8aa8fa4023814902553f939203cd150c918b8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49afcec6e1ad211e0201c2cf51a4d6a439c8a679")]
 [assembly: System.Reflection.AssemblyProductAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Talabat.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
